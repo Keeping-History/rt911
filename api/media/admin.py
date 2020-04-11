066ff06b9dac5d8360e7673368cb39f9021aba20
Robbie Byrd <robbie.byrd@t-3.com>
2020-04-11 13:18:11 -0500

    Did a little work to the admin side
    Signed-off-by: Robbie Byrd <robbie.byrd@t-3.com>

--- api/media/admin.py
+++ api/media/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Media
 
-admin.site.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ['approved', 'duration', 'title' ,'start_date', 'end_date', 'source', 'tz', 'format', 'mediaType']
+    list_filter = ['approved']
+    search_fields = ['full']
+
+admin.site.register(Media, MediaAdmin)
--- api/media/models.py
+++ api/media/models.py
@@ -12,6 +12,26 @@ class Media(models.Model):
     url         = models.URLField()
     format      = models.CharField(max_length=10)
     full        = models.CharField(max_length=255)
+    approved    = models.BooleanField(default=False, verbose_name= u"\U0001F44D")
+
+    approved.boolean = True
 
     def __str__(self):
         return self.full
+
+    def mediaType(self):
+        mediaTypes = {
+            'video': set(['h.264', 'mp4', 'mov', 'mpg', 'webm', 'ogg']),
+            'audio': set(['mp3', 'aac', 'ogg', 'flac', 'webm', 'wav']),
+            'html': set(['html']),
+            'iframe': set(['iframe']),
+            'image': set(['jpg', 'png', 'gif']),
+        }
+        for mediaType in mediaTypes:
+            if self.format in mediaTypes[mediaType]:
+                return mediaType
+            else:
+                return self.format
+
+    def duration(self):
+            return self.end_date - self.start_date
