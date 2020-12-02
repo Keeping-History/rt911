6b6c905064a92e113b3c77e4595192028d80dc32
Robbie Byrd <robbie.byrd@t-3.com>
2020-12-02 12:08:49 -0600

    All the changes...
    Signed-off-by: Robbie Byrd <robbie.byrd@t-3.com>

--- gdeploy.sh
+++ gdeploy.sh
@@ -1,4 +1,6 @@
+./start_db.sh
+sleep 5
 bash -c "python3 manage.py makemigrations &&
 python3 manage.py migrate &&
 python3 manage.py shell -c 'from django.contrib.auth.models import User; User.objects.filter(email=\"rt911@robbiebyrd.com\").delete(); User.objects.create_superuser(\"rt911\", \"rt911@robbiebyrd.com\", \"rt911\")'"
-gcloud app deploy
+gcloud app deploy -q
