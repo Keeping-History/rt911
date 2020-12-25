35011b2d528ca5d2a132fae69c07e08ded4e5945
Robbie Byrd <robbie.byrd@t-3.com>
2020-12-24 21:50:41 -0600

    Untrack

--- backend/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/robbie.byrd/Library/Mobile Documents/com~apple~CloudDocs/Sites/rt911/backend/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
