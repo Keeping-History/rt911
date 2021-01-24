47b8ebe2256c4f77a6db1f94f5f1fc81625f0def
Robbie Byrd <robbie.byrd@t-3.com>
2021-01-23 22:00:41 -0600

    Use jQuery() instead of $() to avoid conflicts
    Signed-off-by: Robbie Byrd <robbie.byrd@t-3.com>

--- frontend/index.js
+++ frontend/index.js
@@ -1,117 +1,115 @@
-$(function () {
+jQuery(function () {
     //Make windows movable and make sure the active window is on top
-    $(".draggable-window").draggable({
+    jQuery(".draggable-window").draggable({
         handle: "h1.title",
         start: function () {
-            $(".content").css("z-index", "1100");
-            $(this).css("z-index", "1200");
+            jQuery(".content").css("z-index", "1100");
+            jQuery(this).css("z-index", "1200");
         },
     });
 
     //Make icons movable
-    $(".draggable-icon").draggable();
+    jQuery(".draggable-icon").draggable();
 
     //Make windows resizable
     //TODO: This is not currently working because we don't have a grab button
-    $(".resizable").resizable();
+    jQuery(".resizable").resizable({
+        handles: "se"
+    });
 
     //Enable Icons and Menu items to be clickable and open their windows/apps
-    $(".icon").dblclick(function () {
-        $("#" + $(this).get(0).id.split("-")[1]).removeClass('hidden').css("z-index", "9000");
+    jQuery(".icon").dblclick(function () {
+        jQuery("#" + jQuery(this).get(0).id.split("-")[1]).removeClass('hidden').css("z-index", "9000");
     })
 
     // Zoom Box -- Make Window Full Screen and toggle back
-    $(".zoom-box").on("click", function () {
+    jQuery(".zoom-box").on("click", function () {
         b = this.closest(".content");
-        isMax = $(this).data("max");
+        isMax = jQuery(this).data("max");
 
         if (!isMax) {
-            $(b)
+            jQuery(b)
                 .css("width", "99%")
                 .css("height", "95%")
                 .css("left", ".5rem")
                 .css("top", "2.5rem");
 
-            $(this)
-                .data("width", $(b).css("width"))
-                .data("height", $(b).css("height"))
-                .data("top", $(b).css("top"))
-                .data("left", $(b).css("left"))
+            jQuery(this)
+                .data("width", jQuery(b).css("width"))
+                .data("height", jQuery(b).css("height"))
+                .data("top", jQuery(b).css("top"))
+                .data("left", jQuery(b).css("left"))
                 .data("max", true);
 
         } else {
-            $(b)
-                .css("width", $(this).data("width"))
-                .css("height", $(this).data("height"))
-                .css("top", $(this).data("top"))
-                .css("left", $(this).data("left"))
+            jQuery(b)
+                .css("width", jQuery(this).data("width"))
+                .css("height", jQuery(this).data("height"))
+                .css("top", jQuery(this).data("top"))
+                .css("left", jQuery(this).data("left"))
                 .css("z-index","900")
 
-            $(this)
+            jQuery(this)
                 .data("max", false);
 
         }
     });
 
     // Windowshade Box -- Minimize the window to just the title bar
-    $(".windowshade-box").on("click", function () {
+    jQuery(".windowshade-box").on("click", function () {
         c = this.closest(".content");
         c.css("z-index", "1");
-        d = $(this).data("shade");
-        e = $(this).data("shade-height", $(this).css("height"));
+        d = jQuery(this).data("shade");
+        e = jQuery(this).data("shade-height", jQuery(this).css("height"));
 
         if (!d) {
-            $(c).children('.inner').addClass('hidden');
-            $(c).css('height', "");
-            $(this).data("shade", true);
+            jQuery(c).children('.inner').addClass('hidden');
+            jQuery(c).css('height', "");
+            jQuery(this).data("shade", true);
 
         } else {
-            $(c).children('.inner').removeClass('hidden');
-            $(c).css("height", e);
-            $(this).data("shade", false);
+            jQuery(c).children('.inner').removeClass('hidden');
+            jQuery(c).css("height", e);
+            jQuery(this).data("shade", false);
         }
 
     });
     
     // Close Box -- Close the window when clicked
-    $(".close-box, .close-button").on("click", function () {
+    jQuery(".close-box, .close-button").on("click", function () {
         a = this.closest(".content");
-        $(a).addClass('hidden');
+        jQuery(a).addClass('hidden');
     });
 
     // Make sure the active window is on top
-    $(".content").click(function () {
-        $(".content").css("z-index", "1100")
-        $(this).css("z-index", "1200");
+    jQuery(".content").click(function () {
+        jQuery(".content").css("z-index", "1100")
+        jQuery(this).css("z-index", "1200");
     });
 
-    $("#menu-tv").click(function () {
-        $("#tv").removeClass('hidden').css("z-index", "9000");;
+    // Menu Items - show and hide menu items and drop downs
+    jQuery("#nav-list li").on("mouseenter", function () {
+        jQuery(this).children().show();
     });
 
-    $("#menu-news").click(function () {
-        $("#news").removeClass('hidden').css("z-index", "9000");;
-        setReadMores();
+    jQuery("#nav-list li").on("mouseleave", function () {
+        jQuery(this).children("ul").hide();
     });
 
-    $("#menu-audio").click(function () {
-        $("#audio").removeClass('hidden').css("z-index", "9000");;
+    jQuery("#nav-list li").on("click", function () {
+        jQuery(this).children("ul").hide();
     });
 
-    $("#menu-about").click(function () {
-        $("#about").removeClass('hidden').css("z-index", "9000");
-    });
+    // Enable menu items to be clickable by default and open a windows/app with the same name
+    jQuery("#nav-list li ul li").click(function () {
+        jQuery("#" + jQuery(this).get(0).id.split("-")[1]).removeClass('hidden').css("z-index", "9000");
+    })
 
-    $("#menu-play").click(function () {
-        $("#timekeeper").trigger('play');
-    });
-
-    $("#menu-pause").click(function () {
-        $("#timekeeper").trigger('pause');
-    });
-
-    $("#menu-settings").click(function () {
-        $("#settings").removeClass('hidden');
+    jQuery('#modalBoot').modal({
+        backdrop: true,
+        show: true,
+        showClose: false,
+        fadeDuration: 250
     });
 
 });
