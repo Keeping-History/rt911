ea1d320c772376db68f1644538a34dee54f8afd2
Robbie Byrd <robbie.byrd@t-3.com>
2021-08-25 15:02:59 -0500

    Fix start_server command to match docs

--- frontend/start_server.sh
+++ frontend/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 -m http.server --bind 127.0.0.1 8000 &>/dev/null &
-open http://127.0.0.1:8000
+python3 -m http.server --bind 127.0.0.1 8001 &>/dev/null &
+open http://127.0.0.1:8001
 
