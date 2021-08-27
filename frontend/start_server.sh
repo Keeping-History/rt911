a6fb3cae81bb1aabed0a790ccf19e6c8db20c6cd
Robbie Byrd <robbie.byrd@t-3.com>
2021-08-26 21:02:56 -0500

    Fix prot for frontend server

--- frontend/start_server.sh
+++ frontend/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 -m http.server --directory build --bind 127.0.0.1 8001 &>/dev/null &
-open http://127.0.0.1:8001
+python3 -m http.server --directory build --bind 127.0.0.1 8000 &>/dev/null &
+open http://127.0.0.1:8000
