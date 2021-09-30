0099b64297754a31d934ac2e66c6fc3b984610ae
Robbie Byrd <robbie.byrd@t-3.com>
2021-09-30 00:42:51 -0500

    Update deploy.sh file for backend

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ./start_db.sh
 sleep 5
-bash -c "python3 manage.py makemigrations &&
+bash -c "pip3 install -r requirements.txt &&
+python3 manage.py makemigrations &&
 python3 manage.py collectstatic --noinput &&
 python3 manage.py migrate &&
 python3 manage.py shell -c 'from django.contrib.auth.models import User; User.objects.filter(email=\"rt911@robbiebyrd.com\").delete(); User.objects.create_superuser(\"rt911\", \"rt911@robbiebyrd.com\", \"rt911\")'"
